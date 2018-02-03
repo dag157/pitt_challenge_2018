420aec26e00dc8ca04cf0d2d161bc4c756434967
Dominick Gurnari <dag157@pitt.edu>
2018-02-03 18:46:37 -0500

    website

---
+++ scrpitt.js
@@ -0,0 +1,12 @@
+function Validate() {
+                //retreive data
+    var input1 = document.getElementById("input1");
+    var input2 = document.getElementById("input2");
+               var input3 = document.getElementById("input3");
+    
+                var input4 = document.getElementById("input4");
+    
+    document.getElementById("myInput").innerHTML = input1.value;
+
+
+}
